a72a4436b7504fdf4a9ed192e2ad065721f04694
choiboa <musamea99@gmail.com>
2025-09-17 01:46:30 +0900

    📝 Chore : RTK 초기 세팅

---
+++ src/store/authSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+interface AuthState {
+  user: null | { uid: string; email: string | null; token?: string };
+}
+
+const initialState : AuthState ={ user: null }
+
+const authSlice= createSlice({
+  name : 'auth',
+  initialState,
+  reducers: {
+    setUser : (state, action: PayloadAction<AuthState["user"]>) => {
+      state.user = action.payload;
+    },
+    clearUser:(state) => {
+      state.user = null
+    }
+  }
+})
+
+export const { setUser, clearUser } = authSlice.actions;
+export default authSlice.reducer;
