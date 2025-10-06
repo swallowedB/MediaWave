6e9b37b63424b8d86e8de2603d99e8a1b692b933
choiboa <musamea99@gmail.com>
2025-10-06 10:58:30 +0900

    📝 Chore : lazy 로딩 방식 적용

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,48 +1,60 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import NotFound from "../pages/NotFound";
-import Login from "../pages/auth/Login";
-import Browse from "../pages/browse/Browse";
-import Home from "../pages/home/Home";
-import BaseLayout from "./layouts/BaseLayout";
-import { fetchHomeData } from "./loader/home.loader";
-import { fetchBrowse } from "./loader/browse.loader";
 import ErrorState from "../components/common/ErrorState";
-import MyPage from "../pages/my/MyPage";
+import NotFound from "../pages/NotFound";
+import BaseLayout from "./layouts/BaseLayout";
 import { redirectIfAuth, requireAuth } from "./loader/auth.loader";
-import MediaDetail from "../pages/detail/MediaDetail";
+import { fetchBrowse } from "./loader/browse.loader";
 import { mediaDetailLoader } from "./loader/detail.loader";
+import { fetchHomeData } from "./loader/home.loader";
+import Loading from "../components/common/Loading";
 
 const router = createBrowserRouter([
   {
     Component: BaseLayout,
     errorElement: <ErrorState />,
+    hydrateFallbackElement: <Loading />,
     children: [
       {
         path: "/",
         loader: fetchHomeData,
-        Component: Home,
+        lazy: () =>
+          import("../pages/home/Home").then((m) => ({
+            Component: m.default,
+          })),
       },
       {
         path: "/browse",
         loader: fetchBrowse,
-        Component: Browse,
+        lazy: () =>
+          import("../pages/browse/Browse").then((m) => ({
+            Component: m.default,
+          })),
       },
       {
         path: "/detail/:type/:id",
         loader: mediaDetailLoader,
-        Component: MediaDetail,
+        lazy: () =>
+          import("../pages/detail/MediaDetail").then((m) => ({
+            Component: m.default,
+          })),
       },
     ],
   },
   {
     path: "/my",
     loader: requireAuth,
-    Component: MyPage,
+    lazy: () =>
+      import("../pages/my/MyPage").then((m) => ({
+        Component: m.default,
+      })),
   },
   {
     path: "/login",
     loader: redirectIfAuth,
-    Component: Login,
+    lazy: () =>
+      import("../pages/auth/Login").then((m) => ({
+        Component: m.default,
+      })),
   },
   {
     path: "*",
